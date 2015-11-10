ca2c6ab595ce48ac5bfc795d4f25b31166a67ab2
Gagoi <harrygag@gmail.com>
2015-11-10 23:20:31 +0100

    Ajout du serverSocket, et d'un port.

--- fr/cop/server/core/Server.java
+++ fr/cop/server/core/Server.java
@@ -1,12 +1,22 @@
 package fr.cop.server.core;
 
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.UnknownHostException;
 
 public class Server {
 
+	private ServerSocket serverSocket;
+	private int port = 163;
+	
 	public Server() {
 		printServInfo();
+		try {
+			serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String args[]) {
@@ -19,11 +29,11 @@ public class Server {
 		System.out.println("-----------------------------------");
 		System.out.println("Running Information :");
 		try {
-			System.out.println(" - IP : * Local : " + InetAddress.getLocalHost().getHostAddress() + "\r        * Online : " + InetAddress.getByName("www.google.com").getHostAddress());
+			System.out.println(" - IP Local : " + InetAddress.getLocalHost().getHostAddress());
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
-		System.out.println(" - Port : ");
+		System.out.println(" - Port : " + port);
 		//		System.out.println(" - RAM : " + ((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())/1000000) + "/" + Runtime.getRuntime().totalMemory()/1000000);
 		System.out.println(" - Processor avaible : " + Runtime.getRuntime().availableProcessors());
 		System.out.println("-----------------------------------");
